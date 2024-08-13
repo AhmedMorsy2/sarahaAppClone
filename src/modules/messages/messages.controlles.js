1ac4371cbde2008b1f1d7fd4582ca36c0c94c4ea
AhmedMorsy <20192234@fue.edu.eg>
2024-08-14 02:18:43 +0300

    main

--- src/modules/messages/messages.controlles.js
+++ src/modules/messages/messages.controlles.js
@@ -1,8 +1,19 @@
-const message = (req, res) => {
-  console.log(req.session);
+import QRCode from "qrcode";
+
+const message = async (req, res) => {
+  let url = `${req.protocol}://${req.get("host")}/user`;
+  let qrCodeUrl;
+  await QRCode.toDataURL(url)
+    .then((url) => {
+      qrCodeUrl = url;
+      console.log(url);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
   if (req.session.isLoggedIn) {
-    res.render("message.ejs", { session: req.session });
+    res.render("message.ejs", { session: req.session, url, qrCodeUrl });
   } else {
     redirect("/login");
   }
