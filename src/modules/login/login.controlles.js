a3eee23fbac13b9379c55ed5713d11b29a877a98
AhmedMorsy <20192234@fue.edu.eg>
2024-08-13 21:30:41 +0300

    main

--- src/modules/login/login.controlles.js
+++ src/modules/login/login.controlles.js
@@ -1,8 +1,16 @@
+import { User } from "../../../Database/model/user.model.js";
+import bcrypt from "bcrypt";
 const login = (req, res) => {
   res.render("login.ejs");
 };
 
-const handleLogin = (req, res) => {
+const handleLogin = async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (!user || !bcrypt.compareSync(req.body.password, user.password)) {
+    console.log("incorrect");
+
+    return res.redirect("/login");
+  }
   res.redirect("/messages");
 };
 
